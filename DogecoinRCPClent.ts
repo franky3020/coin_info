ed6ab913bc12a7e20ac932695fd668e5a532561e
franky3020 <210238hwsh@gmail.com>
2024-08-18 19:45:47 +0800

    feat: 新增拿取區塊數的api

--- DogecoinRCPClent.ts
+++ DogecoinRCPClent.ts
@@ -1,12 +1,53 @@
-class DogecoinRCPClent {
+import axios from 'axios';
+import dotenv from "dotenv";
+
+export class DogecoinRCPClent {
 
 
-    GetBlockCount(){
+    GetBlock(blockHash: string){
+        const url = process.env.NODE_URL as string;
 
+        const auth = {
+            username: process.env.NODE_USER_NAME as string,
+            password: process.env.NODE_PW as string
+        };
+        const data = {
+            method: "getblock",
+            params: [blockHash, 2]
+        };
+
+        axios.post(url, data, {
+            auth: auth
+        })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     }
 
+    GetBlockCount(): Promise<number>{
+        const url = process.env.NODE_URL as string;
 
-
-
-
+        const auth = {
+            username: process.env.NODE_USER_NAME as string,
+            password: process.env.NODE_PW as string
+        };
+        const data = {
+            method: "getblockcount",
+            params: []
+        };
+        return new Promise<number>((resolve, reject) => {
+            axios.post(url, data, {
+                auth: auth
+            })
+            .then(response => {
+                resolve(response.data.result as number);
+            })
+            .catch(error => {
+                reject();
+            });
+        })
+    }
 }
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { DogecoinRCPClent } from "./DogecoinRCPClent";
 
 // configures dotenv to work in your application
 dotenv.config();
@@ -7,8 +8,18 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+const dogecoinRCPClent = new DogecoinRCPClent();
+
+// dogecoinRCPClent.GetBlock("9c7f36f170ccb64630fbca8d9a665f4a439a3698dd1f6cb06b619ae6c7c76dd5");
+// dogecoinRCPClent.GetBlockCount();
+
 app.get("/", (request: Request, response: Response) => {
   response.status(200).send("Hello World");
+});
+
+app.get("/BlockCount", async (request: Request, response: Response) => {
+  let count = await dogecoinRCPClent.GetBlockCount();
+  response.status(200).send(count.toString());
 });
 
 app.listen(PORT, () => {
